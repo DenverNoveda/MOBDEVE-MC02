07b710e253c7665ad6dd11cac53527215e6c1ba1
itsmedenver <68907210+itsmedenver@users.noreply.github.com>
2021-05-19 23:23:35 +0800

    Edited Movie.java and User.java

--- app/src/main/java/com/mobdeve/s13_demesa_noveda/mobdeve_mc02/Movie.java
+++ app/src/main/java/com/mobdeve/s13_demesa_noveda/mobdeve_mc02/Movie.java
@@ -1,4 +1,13 @@
 package com.mobdeve.s13_demesa_noveda.mobdeve_mc02;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Movie {
+    private String movieName;
+    private ArrayList<String> cast;
+    private Date releaseDate;
+    private String summary;
+    private ArrayList<Movie> recommendations;
+    private ArrayList<Review> movieReviews;
 }
--- app/src/main/java/com/mobdeve/s13_demesa_noveda/mobdeve_mc02/User.java
+++ app/src/main/java/com/mobdeve/s13_demesa_noveda/mobdeve_mc02/User.java
@@ -1,4 +1,10 @@
 package com.mobdeve.s13_demesa_noveda.mobdeve_mc02;
 
+import java.util.ArrayList;
+
 public class User {
+    private String username;
+    private String password;
+    private String email;
+    private ArrayList<Review> reviewList;
 }
