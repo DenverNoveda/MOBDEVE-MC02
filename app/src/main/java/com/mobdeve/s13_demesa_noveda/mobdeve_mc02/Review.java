5a03f0e25db285b0f0a16d08e3519f438fad9c61
itsmedenver <68907210+itsmedenver@users.noreply.github.com>
2021-05-19 23:25:18 +0800

    Edited Review.java

--- app/src/main/java/com/mobdeve/s13_demesa_noveda/mobdeve_mc02/Review.java
+++ app/src/main/java/com/mobdeve/s13_demesa_noveda/mobdeve_mc02/Review.java
@@ -1,4 +1,10 @@
 package com.mobdeve.s13_demesa_noveda.mobdeve_mc02;
 
+import java.util.Date;
+
 public class Review {
+    private String reviewMovieName;
+    private String reviewContent;
+    private Date reviewPublished;
+    private User reviewAuthor;
 }
