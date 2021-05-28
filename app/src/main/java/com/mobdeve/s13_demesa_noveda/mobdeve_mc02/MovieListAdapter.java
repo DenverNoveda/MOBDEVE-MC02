54a8682efa59416d77e037d911a981b9c2f30aff
itsmedenver <68907210+itsmedenver@users.noreply.github.com>
2021-05-28 21:24:05 +0800

    Added MovieListAdapter

---
+++ app/src/main/java/com/mobdeve/s13_demesa_noveda/mobdeve_mc02/MovieListAdapter.java
@@ -0,0 +1,57 @@
+package com.mobdeve.s13_demesa_noveda.mobdeve_mc02;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MyViewHolder> {
+    private ArrayList<Movie> data;
+    public MovieListAdapter(ArrayList data){this.data = data;}
+
+    public int getItemCount(){return data.size();}
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        private TextView tv_listItemName;
+        private ImageView iv_listItemPoster;
+
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
+            this.iv_listItemPoster = itemView.findViewById(R.id.iv_listItemPoster);
+            this.tv_listItemName = itemView.findViewById(R.id.tv_listItemName);
+        }
+
+        public void setMovieName(String title){
+            this.tv_listItemName.setText(title);
+        }
+        public void setMoviePoster(int position){
+            Picasso.get().load(data.get(position).getImage()).into(iv_listItemPoster);
+            iv_listItemPoster.setImageAlpha(255);
+        }
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.movie_list_item, parent, false);
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Log.v("Movie Name: ",  data.get(position).getMovieName());
+        holder.setMovieName(data.get(position).getMovieName());
+        holder.setMoviePoster(position);
+    }
+
+}
